3c18281db0b492aa9c95d0d019482979d69327bb
カルイカネMk22\kazuma <yahatakazuma@gmail.com>
2020-11-14 22:43:48 +0900

    ボタンをスクリプトから実行できるように変えた。

--- Assets/Scripts/Map_Data.cs
+++ Assets/Scripts/Map_Data.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //ただのマップクラス。
 public class Province
@@ -21,6 +22,13 @@ public class Province
     private int supply_count;
     private int fuel_count;
 
+    //位置情報
+    private float posi_x;
+    private float posi_y;
+    //private string name_on_map;
+
+    
+
 
     /// <summary>
     /// プロヴィンスのコンストラクタ
@@ -32,7 +40,8 @@ public class Province
     /// <param name="supply"></param>
     public Province(int id, string name, string owner, string unit
         , int mil_pt_size, int mil_pt_damage, int pri_pt_size, int pri_pt_damage
-        , int supply, int fuel)
+        , int supply, int fuel
+        , float x ,float y)
     {
         Provincial_id = id;
         provincial_name = name;
@@ -49,6 +58,8 @@ public class Province
         supply_count = supply;
         fuel_count = fuel;
 
+        posi_x = x;
+        posi_y = y;
 
     }
 
@@ -64,10 +75,36 @@ public class Province
     public int Pri_port_damage { get => pri_port_damage; set => pri_port_damage = value; }
     public int Supply_count { get => supply_count; set => supply_count = value; }
     public int Fuel_count { get => fuel_count; set => fuel_count = value; }
+    public float Posi_x { get => posi_x; set => posi_x = value; }
+    public float Posi_y { get => posi_y; set => posi_y = value; }
+    
 
     public void Add_test()
     {
         provincial_stock_metal += 20;
+    }
+
+}
+
+//プロヴィンスのUI。
+public class UI_Province
+    {
+    private int provincial_id;
+    private float posi_x;
+    private float posi_y;
+    private string name_on_map;
+
+    public int Provincial_id { get => provincial_id; set => provincial_id = value; }
+    public float Posi_x { get => posi_x; set => posi_x = value; }
+    public float Posi_y { get => posi_y; set => posi_y = value; }
+    public string Name_on_map { get => name_on_map; set => name_on_map = value; }
+
+    public UI_Province(int id ,float x,float y , string name)
+    {
+        provincial_id = id;
+        posi_x = x;
+        posi_y = y;
+        name_on_map = name;
     }
 
 }
@@ -79,6 +116,9 @@ public class Map_Data : MonoBehaviour
     [SerializeField]
     GameObject panel_provi;
 
+    [SerializeField]
+    GameObject Map;
+
 
     //プロヴィンスの入ったリスト。　TODO　DBとやり取りするようにする。
     private List<Province> list_province = new List<Province>();
@@ -87,16 +127,29 @@ public class Map_Data : MonoBehaviour
     void Start()
     {
         Deploy();
+   
+        
     }
 
     public void Deploy()
     {
-        Province p1 = new Province(1, "シンガポール", "イングランド王国", "南方軍", 10, 8, 10, 4, 40, 50);
-        Province p2 = new Province(2, "ロンディニウム", "イングランド王国", "南方軍", 6, 3, 5, 33, 600, 330);
+        Province p1 = new Province(1, "シンガポール", "イングランド王国", "南方軍", 10, 8, 10, 4, 40, 50,-505,20);
+        CreateButton(p1);
 
+        Province p2 = new Province(2, "ブルネイ", "イングランド王国", "南方軍", 6, 3, 5, 33, 600, 330,-205,77);
+        CreateButton(p2);
+        //Province p3 = new Province(3, "3", "イングランド王国", "南方軍", 6, 3, 5, 33, 600, 330);
+        //Province p4 = new Province(4, "4", "イングランド王国", "南方軍", 6, 3, 5, 33, 600, 330);
+        //Province p5 = new Province(5, "5", "イングランド王国", "南方軍", 6, 3, 5, 33, 600, 330);
+        //Province p6 = new Province(6, "6", "イングランド王国", "南方軍", 6, 3, 5, 33, 600, 330);
+        //Province p7 = new Province(7, "7", "イングランド王国", "南方軍", 6, 3, 5, 33, 600, 330);
+        //Province p8 = new Province(8, "8", "イングランド王国", "南方軍", 6, 3, 5, 33, 600, 330);
+        //Province p9 = new Province(9, "9", "イングランド王国", "南方軍", 6, 3, 5, 33, 600, 330);
+        //Province p10 = new Province(10, "10", "イングランド王国", "南方軍", 6, 3, 5, 33, 600, 330);
 
         list_province.Add(p1);
         list_province.Add(p2);
+        //list_province.Add(p4);
     }
 
 
@@ -126,6 +179,42 @@ public class Map_Data : MonoBehaviour
             );
 
     }
+    
+
+    //ボタンの作成
+    public void CreateButton(Province provi)
+    {
+        //ボタンのプレファブを作成
+        GameObject provi_button = (GameObject)Resources.Load("Prefabs/Map_Button");
+
+        //インスタンス化
+        GameObject puro_button = Instantiate(provi_button, Map.transform);
+
+        //オブジェクト名を変更
+        puro_button.name = "province_" + provi.Provincial_name ;
+
+        //ボタン配下にあるテキストを更新。
+        puro_button.GetComponent<Change_button_name>().Update_Button_Name(provi.Provincial_name);
+
+        //座標を指定する。これは引数から取るようにする。
+        //アクセサー取得。
+        //ローカル座標で指定。todo//もうちょっと一般的な書きぶりを考えること。
+        Vector3 posi = puro_button.transform.localPosition;
+
+        //座標を指定。
+        posi.x = provi.Posi_x;
+        posi.y = provi.Posi_y;
+        posi.z = 0; //guiなのでz面はゼロ。
+        //
+        puro_button.transform.localPosition = posi;
+
+        //ボタンから呼び出す関数を設定。
+        Button button = puro_button.GetComponent<Button>();
+        //ブタンが持つ関数を取得。引数に注意。
+        button.onClick.AddListener(() => Pick_Province(provi.Provincial_id));
+
+    }
+
 
 
 }
---
+++ Assets/Scripts/common/Change_button_name.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Change_button_name : MonoBehaviour
+{
+    [SerializeField]
+    Text buttun_name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //ボタン配下のテキストを変更。
+    public void Update_Button_Name(string name)
+    {
+        buttun_name.text = name;
+
+    }
+
+}
